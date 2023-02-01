33c1d92dafed8220fcae1513c2985e0ae4b10bf1
pranavmahale22@gmail.com <pranavmahale22@gmail.com>
2023-02-01 22:51:33 +0530

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '9fypxu',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
